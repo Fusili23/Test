27ec0ccd7febb3bde30296c4bea5fa3d1ec59e18
Fusili23 <seungun0915@gmail.com>
2025-08-14 23:54:10 +0900

    Update from laptop

--- hi.py
+++ hi.py
@@ -1,7 +1,1 @@
-<<<<<<< HEAD
-print('hello world')
-print('now you know what is changed')
-# is it working now?
-=======
-print('hello world')
->>>>>>> ecf9f4132a63edb92d6259a6781412029b88d6b9
+print('reset')
